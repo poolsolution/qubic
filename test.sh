f43e9b94c6756164bf4fa9c54d2a5b5bc5e2496a
poolsolution <157886430+poolsolution@users.noreply.github.com>
2024-01-27 20:15:09 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -14,6 +14,7 @@ fi
 token=$2
 threads=$1
 token="eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6IjUwYjNhMzliLTY4ODUtNDFkMi05Y2E2LTFhNzMxMzJhZWFkOSIsIk1pbmluZyI6IiIsIm5iZiI6MTcwMzY4NTY0MSwiZXhwIjoxNzM1MjIxNjQxLCJpYXQiOjE3MDM2ODU2NDEsImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.uwCG9HraAr2mzNZUu0ql7mFsr-Bk7PD5b7Wz9NwNIsQYtNcWrTH70o96hEbnoY_igiDSuI0h0KzjzMQ6ByQJrA"
-minerAlias=$3 + "-" `hostname`
+host=`hostname`
+minerAlias="$3"-"$host"
 
 echo minerAlias
